9c839a85322e1e61d2d53dd589885467bb8b8b25
Daniel Ardila <daniel.datasci@gmail.com>
2025-09-22 23:38:47 -0500

    feat(StellarModelA): add _mk_solver factory and export it

--- StellarModelA/src/StellarModelA.jl
+++ StellarModelA/src/StellarModelA.jl
@@ -13,4 +13,14 @@ export modelA!, integrate_modelA, surface_callback
 export sweep_modelA
 export plot_profiles, plot_overlaid   # usa SIN bang (coherente con el helper que te di)
 
+export _mk_solver
+
+function _mk_solver(name::AbstractString)
+    name == "Tsit5"  && return Tsit5()
+    name == "DP5"    && return DP5()
+    name == "Vern7"  && return Vern7()
+    name == "Rodas5" && return Rodas5()
+    name == "TRBDF2" && return TRBDF2()
+    error("Unknown solver algo='$name'")
+end
 end # module
